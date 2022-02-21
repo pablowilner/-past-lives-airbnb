c321619931acdbd3af14e02f32b9b5c38d7b4500
Lennart von Velsen <lennartvonvelsen@gmail.com>
2022-02-21 18:02:59 +0100

    Characters Controller

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,12 @@
 class CharactersController < ApplicationController
+  def index
+    @characters = Character.all
+  end
+
+  def new
+    @character = Character.new
+  end
+
+  def update
+  end 
 end
