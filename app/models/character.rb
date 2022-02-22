cb4132a3572c7be0707f489da5857502585ab3e4
Pablo Wilner <pablowilner@gmail.com>
2022-02-22 12:10:43 +0100

    controller

--- app/models/character.rb
+++ app/models/character.rb
@@ -7,7 +7,7 @@ class Character < ApplicationRecord
   validates :name, presence: true
   validates :profession, presence: true, inclusion: { in: %w(royalty sports celebrity business scientist heroes artists)}
   validates :historical_period, presence: true, inclusion: { in: %w(middle_age 20th_century ancient_history )}
-  validates :location, presence: true
+  # validates :location, presence: true
   validates :price, presence: true
 
 end
