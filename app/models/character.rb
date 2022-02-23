50ae857d55d4d07b74b778046f72b72f2a30de5b
Pablo Wilner <pablowilner@gmail.com>
2022-02-23 10:24:26 +0100

    categories

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,7 +6,7 @@ class Character < ApplicationRecord
   validates :description, presence: true
   validates :name, presence: true
   validates :profession, presence: true, inclusion: { in: %w(royalty sports celebrity business scientist heroes artists)}
-  validates :historical_period, presence: true, inclusion: { in: %w( Ancient _Ages_(Up_to_500) Middle_Ages_(500_1500) Modern_Era_(1500 _1900) Contemporary_History_(1900_Present) )}
+  validates :historical_period, presence: true, inclusion: { in: ["Ancient Ages (Up to 500)", "Middle Ages (500 1500)", "Modern Era (1500 1900)", "Contemporary History (1900 Present)"] }
   # validates :location, presence: true
   validates :price, presence: true
   validates :photo, presence: true
