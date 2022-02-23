0bf38f8f4da0b6141f1cfad3270108cc471bd763
ana rebelo <anarebelosoares@gmail.com>
2022-02-23 14:55:03 +0100

    url for images in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,13 @@ require "open-uri"
 
 user = User.create(email: "admin@admin.com", password: "123456")
 
-file_one = URI.open('https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Ingres%2C_Napoleon_on_his_Imperial_throne.jpg/730px-Ingres%2C_Napoleon_on_his_Imperial_throne.jpg?20210129234315')
+file_one = URI.open('https://static.dw.com/image/57410623_303.jpg')
 character_one = Character.new(name: "Napoleon", user: user, title: "Napoleon Coronation 1804", description: "Napoleon's was a sacred ceremony held in the great cathedral of Notre Dame de Paris in the presence of Pope Pius VII. Napoleon brought together various rites and customs, incorporating ceremonies of Carolingian tradition, the Ancien Régime and the French Revolution, all presented in sumptuous luxury. On May 18, 1804, the Sénat conservateur vested the Republican government of the French First Republic in an emperor, and preparations for a coronation followed. Napoleon's elevation to emperor was overwhelmingly approved by the French citizens in the French constitutional referendum of 1804. Among Napoleon's motivations for being crowned were to gain prestige in international royalist and Catholic circles and to lay the foundation for a future dynasty.",
                           price: 200, historical_period: "Modern Era (1500 1900)", profession: "royalty", address: "Notre-Dame de Paris, Paris")
 character_one.photo.attach(io: file_one, filename: 'napoleon.jpg', content_type: 'image/jpg')
 character_one.save
 
-file_two = URI.open('https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Jacques-Louis_David_-_The_Emperor_Napoleon_in_His_Study_at_the_Tuileries_-_Google_Art_Project.jpg/1200px-Jacques-Louis_David_-_The_Emperor_Napoleon_in_His_Study_at_the_Tuileries_-_Google_Art_Project.jpg')
+file_two = URI.open('https://static.dw.com/image/57410623_303.jpg')
 character_two = Character.new(name: "Napoleon Bonaparte", user: user, title: "The French Revolution (1789) with Napoleon", description: "One of the most celebrated personages in the history of the West. He revolutionized military organization and training; sponsored the Napoleonic Code, the prototype of later civil-law codes; reorganized education; and established the long-lived Concordat with the papacy.",
   price: 150, historical_period: "Modern Era (1500 1900)", profession: "royalty", address: "Paris, France")
 character_two.photo.attach(io: file_two, filename: 'napoleon_two.jpg', content_type: 'image/jpg')
@@ -45,7 +45,7 @@ character_five.photo.attach(io: file_five, filename: 'frida.jpg', content_type: 
 character_five.save
 
 
-file_six = URI.open('https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Billy_the_Kid_corrected.jpg/862px-Billy_the_Kid_corrected.jpg')
+file_six = URI.open('https://static4.abc.es/Media/201501/06/foto-billy-nino--644x362.jpg')
 character_six = Character.new(name: "Billy The Kid", user: user,  title: "Billy the Kid’s Last Jailbreak", description: "In spring of  1881, toward the end of the Lincoln County War, William Bonney was jailed in Lincoln,  having been tried for the murder of Sheriff William Brady.  He would escape one last time from the courthouse jail on April 28th, killing Deputies JW Bell and R. Olinger on his way. ",
   price: 400, historical_period: "Modern Era (1500 1900)", profession: "celebrity", address: "New York, USA")
 character_six.photo.attach(io: file_six, filename: 'frida.jpg', content_type: 'image/jpg')
