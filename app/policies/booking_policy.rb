7dcef6344b8ca8ed02c49b9ac3c95d3b38f6b1dc
Lennart von Velsen <lennartvonvelsen@gmail.com>
2022-02-24 14:40:06 +0100

    Booking Feature implemented in the Calendar

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,17 @@
+class BookingPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+
+  def create?
+    return true
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+end
