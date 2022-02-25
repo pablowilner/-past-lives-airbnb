a4fddcabe0e4da0959c55e397307fe5bf37382b2
Pablo Wilner <pablowilner@gmail.com>
2022-02-25 14:29:58 +0100

    some more changes

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,6 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :booking
   has_one :character, through: :booking
+  validates :content, presence: true
+  validates :rating, presence: true
 end
